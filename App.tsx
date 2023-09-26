487b1e75f709b255e6d7b0a94d75faa13f11e070
venturions <alex.eduardo10@hotmail.com>
2023-09-26 19:57:07 -0300

    Chamando as rotas

--- App.tsx
+++ App.tsx
@@ -9,7 +9,7 @@ import {
 } from '@expo-google-fonts/roboto'
 import { Loading } from '@components/Loading'
 import { THEME } from './src/theme'
-import { SignIn } from '@screens/SignIn'
+import { Routes } from '@routes/index'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
@@ -21,7 +21,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <SignIn /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </NativeBaseProvider>
   )
 }
