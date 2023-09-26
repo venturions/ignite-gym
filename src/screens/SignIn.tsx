0837490a10639570190b8ab56924d924f2a0b257
venturions <alex.eduardo10@hotmail.com>
2023-09-25 21:00:33 -0300

    componente de heading

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,4 +1,4 @@
-import { VStack, Image, Text, Center } from 'native-base'
+import { VStack, Image, Text, Center, Heading } from 'native-base'
 
 import BackgroundImg from '@assets/background.png'
 import LogoSVG from '@assets/logo.svg'
@@ -18,6 +18,12 @@ export function SignIn() {
         <Text color="gray.100" fontSize="sm">
           Treine a sua mente e o seu corpo
         </Text>
+      </Center>
+
+      <Center>
+        <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
+          Acesse sua conta
+        </Heading>
       </Center>
     </VStack>
   )
