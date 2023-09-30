cc9c83cd27218ecbcbadf4a52d92fda40a5e37e4
venturions <alex.eduardo10@hotmail.com>
2023-09-30 11:58:34 -0300

    Componente HistoryCard

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,3 +1,4 @@
+import { HistoryCard } from '@components/HistoryCard'
 import { ScreenHeader } from '@components/ScreenHeader'
 import { VStack } from 'native-base'
 
@@ -5,6 +6,9 @@ export function History() {
   return (
     <VStack flex={1}>
       <ScreenHeader title="Histórico de Exercícios" />
+      <HistoryCard />
+      <HistoryCard />
+      <HistoryCard />
     </VStack>
   )
 }
